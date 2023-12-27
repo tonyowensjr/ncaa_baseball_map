1b201de1411f3955bd00348243bc900a2f5e72e9
Tony Owens <to5@princeton.edu>
2023-12-27 11:35:43 -0500

    added dependencies and functions to acquire the coordinates and make the final dataframe

--- process_data.py
+++ process_data.py
@@ -1,12 +1,15 @@
+from geopy.geocoders import Nominatim
 from selenium import webdriver
 from typing import Tuple
 
+import argparse
+import os
 import pandas as pd
 import random
 import subprocess
 import time
 
-def run_R(year:str) -> None:
+def run_R(year:str,delay:str) -> None:
     """Run the R script to create a dataset of NCAA baseball game data for a given year
 
     Args:
@@ -18,7 +21,7 @@ def run_R(year:str) -> None:
     r_file_path = 'game_data.R'
 
     # Define command line arguments as a list
-    arguments = [year]
+    arguments = [year,delay]
 
     # Build the command that will be executed
     command = [r_script_path, r_file_path] + arguments
@@ -95,3 +98,67 @@ def add_locations(game_data:pd.DataFrame) -> Tuple[pd.DataFrame,dict]:
     location_dict = dict(zip(game_data['key'],game_data['location']))
 
     return game_data,location_dict
+
+def find_coords(game_data:pd.DataFrame) -> dict:
+    """Create a dictionary which maps locations to coordinates.
+
+    Args:
+        game_data (pd.DataFrame): A dataframe with the unique locations
+
+    Returns:
+        dict: A dictionary mapping locations to coordinates
+    """
+
+    unique_locations = game_data.location.unique()
+    geolocator = Nominatim(user_agent="ncaa_map")
+    coords_dict = {}
+
+    # iterate through the locations to find the coordinates of each location
+    for unique_loc in unique_locations:
+        # Add a random delay to avoid being blocked by the geolocator
+        delay = random.random() + 1
+        time.sleep(delay)
+
+        # Try to find the coordinates of the location and add them to the dictionary
+        loc_info = geolocator.geocode(unique_loc)
+        try:
+            lat,lon = loc_info.latitude,loc_info.longitude
+            coords_dict[unique_loc] = (lat,lon)
+        except Exception:
+            continue
+    return coords_dict
+
+def main():
+    # Parse the year from the command line
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--year", help="input csv file")
+    argparser.add_argument("--delay", help="the delay (in seconds) between each request in the R file")
+    args = argparser.parse_args()
+    year = args.year
+    delay = args.delay
+
+    # If the data does not already exist, run the R script to create the dataset of NCAA data
+    if not os.path.exists(f'game_data/df_{year}.csv'):
+        run_R(year,delay)
+
+    # Read in the data and add the locations
+    data = pd.read_csv(f'game_data/df_{year}.csv')
+    game_info,location_dict = add_locations(prepare_locations(data))
+
+    # Find the coordinates of each location
+    coords_dict = find_coords(game_info)
+    data['location'] = (data.home_team + data.neutral_site.astype(str)).map(location_dict)
+    data['coords'] = data.location.map(coords_dict)
+
+    # Create a column with the information to be displayed on the map
+    data['info'] = data.home_team + ' vs. ' + data.away_team + ' ' + '(' + data.home_team_score.astype(str) \
+        + '-' + data.away_team_score.astype(str) + ') ' + data.date 
+    
+    # Create columns for the latitude and longitude
+    data[['latitude', 'longitude']] = pd.DataFrame(data['coords'].tolist(), index=data.index)
+    
+    # Save the data
+    data[['latitude','longitude','info']].drop_duplicates().to_csv(f'game_data/cleaned_df_{year}.csv')
+
+if __name__ == "__main__":
+    main()
